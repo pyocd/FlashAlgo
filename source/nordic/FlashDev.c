84fc79ec2b7b29def1aebc52c5f4c0f6519ae511
sg- <sam.w.grove@gmail.com>
2015-04-06 20:29:10 -0500

    Create nordic yaml files and run AStyle in flash algo source.

--- source/nordic/FlashDev.c
+++ source/nordic/FlashDev.c
@@ -1,5 +1,5 @@
-/* CMSIS-DAP Interface Firmware
- * Copyright (c) 2009-2013 ARM Limited
+/* Flash OS Routines
+ * Copyright (c) 2009-2015 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,21 +14,23 @@
  * limitations under the License.
  */
 
-#include "FlashOS.H"        // FlashOS Structures
+#include "FlashOS.H"
+
+#define FLASH_DRV_VERS (0x0100+VERS)   // Driver Version, do not modify!
+#define DEVICE_NAME    "nRF51822AA 256 KB Flash"
 
 struct FlashDevice const FlashDevice  =  {
-   FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "nRF51822AA 256 KB Flash",  // Device Name
-   ONCHIP,                     // Device Type
-   0x00000000,                 // Flash start address
-   0x00040000,                 // Flash total size (256 KB // + 1 kB)
-   4,                          // Programming Page Size
-   0,                          // Reserved, must be 0
-   0xFF,                       // Initial Content of Erased Memory
-   100,                        // Program Page Timeout 100 mSec
-   3000,                       // Erase Sector Timeout 3000 mSec
-// Specify Size and Address of Sectors
-  0x000400, 0x000000,         // Sector Size  1 KB (256 Sectors)
-//  0x000400, 0x10001000        // Sector Size  1 KB (1 Sector)
-  SECTOR_END                  // Marks end of sector table
+    FLASH_DRV_VERS,             // Driver Version, do not modify!
+    DEVICE_NAME,                // Device Name
+    ONCHIP,                     // Device Type
+    0x00000000,                 // Flash start address
+    0x00040000,                 // Flash total size (256 KB // + 1 kB)
+    4,                          // Programming Page Size
+    0,                          // Reserved, must be 0
+    0xFF,                       // Initial Content of Erased Memory
+    100,                        // Program Page Timeout 100 mSec
+    3000,                       // Erase Sector Timeout 3000 mSec
+    {{0x000400, 0x000000},      // Sector Size  1 KB (256 Sectors)
+//    {0x000400, 0x10001000},     // Sector Size  1 KB (1 Sector)
+    {SECTOR_END}}               // Marks end of sector table
 };
