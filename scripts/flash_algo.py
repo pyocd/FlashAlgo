faeba6663cf38676e67d77f52b739444c1b1caa3
Chris Reed <flit@me.com>
2021-03-21 13:22:57 -0500

    Update scripts.
    - Move templates to subdirectory.
    - Merge in Python 3 support for flash_algo.py from DAPLink.
    - Some small changes to generate_blobs.py, and made it executable.

--- scripts/flash_algo.py
+++ scripts/flash_algo.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
-"""
- mbed
- Copyright (c) 2017-2017 ARM Limited
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-"""
+# Copyright (c) 2017-2019 Arm Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from __future__ import print_function
 import os
@@ -22,12 +19,11 @@ import struct
 import binascii
 import argparse
 import logging
-import StringIO
+from io import BytesIO
 import jinja2
 from collections import namedtuple
 from itertools import count
 
-from elftools.common.py3compat import bytes2str
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
 
@@ -130,8 +126,8 @@ class PackFlashAlgo(object):
         elif fmt == "c":
             blob = self.algo_data[:]
             pad_size = 0 if len(blob) % 4 == 0 else 4 - len(blob) % 4
-            blob = blob + "\x00" * pad_size
-            integer_list = struct.unpack("<" + "L" * (len(blob) / 4), blob)
+            blob = blob + b"\x00" * pad_size
+            integer_list = struct.unpack("<" + "L" * int(len(blob) / 4), blob)
             line_list = []
             for pos in range(0, len(integer_list), group_size):
                 group = ["0x%08x" % value for value in
@@ -166,7 +162,7 @@ class PackFlashAlgo(object):
         template = jinja2.Template(template_text)
         target_text = template.render(data_dict)
 
-        with open(output_path, "wb") as file_handle:
+        with open(output_path, "w") as file_handle:
             file_handle.write(target_text)
 
 
@@ -187,7 +183,7 @@ def _find_sections(elf, name_type_pairs):
     """Return a list of sections the same length and order of the input list"""
     sections = [None] * len(name_type_pairs)
     for section in elf.iter_sections():
-        section_name = bytes2str(section.name)
+        section_name = section.name
         section_type = section["sh_type"]
         for i, name_and_type in enumerate(name_type_pairs):
             if name_and_type != (section_name, section_type):
@@ -261,7 +257,7 @@ class PackFlashInfo(object):
         values = struct.unpack(self.FLASH_DEVICE_STRUCT, data)
 
         self.version = values[0]
-        self.name = values[1].strip("\x00")
+        self.name = values[1].strip(b"\x00")
         self.type = values[2]
         self.start = values[3]
         self.size = values[4]
@@ -311,12 +307,12 @@ class ElfFileSimple(ELFFile):
 
     def __init__(self, data):
         """Construct a ElfFileSimple from bytes or a bytearray"""
-        super(ElfFileSimple, self).__init__(StringIO.StringIO(data))
+        super(ElfFileSimple, self).__init__(BytesIO(data))
         self.symbols = self._read_symbol_table()
 
     def _read_symbol_table(self):
         """Read the symbol table into the field "symbols" for easy use"""
-        section = self.get_section_by_name(b".symtab")
+        section = self.get_section_by_name(".symtab")
         if not section:
             raise Exception("Missing symbol table")
 
@@ -325,7 +321,7 @@ class ElfFileSimple(ELFFile):
 
         symbols = {}
         for symbol in section.iter_symbols():
-            name_str = bytes2str(symbol.name)
+            name_str = symbol.name
             if name_str in symbols:
                 logging.debug("Duplicate symbol %s", name_str)
             symbols[name_str] = SymbolSimple(name_str, symbol["st_value"],
--- scripts/generate_blobs.py
+++ scripts/generate_blobs.py
@@ -1,36 +1,42 @@
-'''
-FlashAlgo
-Copyright (c) 2011-2017 ARM Limited
+#!/usr/bin/env python
+# Copyright (c) 2011-2017 Arm Limited
+# Copyright (c) 2021 Chris Reed
+# 
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# 
+# This script takes the path to file(s) created by an image conversion tool
+# (fromelf or arm-elf-objcopy) as input. These files (bin and text) are used
+# to create flash programming blobs (instruction arrays) which are then
+# loaded into the target MCU RAM. Generates files compatible with C programs
+# and python programs (DAPLink Interface Firmware and pyDAPFlash)
 
-Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-
-This script takes the path to file(s) created by an image conversion tool
-(fromelf or arm-elf-objcopy) as input. These files (bin and text) are used
-to create flash programming blobs (instruction arrays) which are then
-loaded into the target MCU RAM. Generates files compatible with C programs
-and python programs (DAPLink Interface Firmware and pyDAPFlash)
-'''
 import os
 import argparse
 from flash_algo import PackFlashAlgo
 
-# TODO
-# FIXED LENGTH - remove and these (shrink offset to 4 for bkpt only)
-BLOB_HEADER = '0xE00ABE00, 0x062D780D, 0x24084068, 0xD3000040, 0x1E644058, 0x1C49D1FA, 0x2A001E52, 0x4770D1F2,'
-HEADER_SIZE = 0x20
+# Header with a BKPT instruction.
+BLOB_HEADER = '0xE00ABE00, '
+HEADER_SIZE = 0x4
 
 STACK_SIZE = 0x200
+
+TEMPLATES = [
+    ("c_blob.tmpl", "c_blob.c"),
+    ("py_blob_orig.tmpl", "py_blob_orig.py"),
+    ("py_blob.tmpl", "py_blob.py"),
+    ("c_blob_mbed.tmpl", "c_blob_mbed.c")
+]
 
 def str_to_num(val):
     return int(val,0)  #convert string to number and automatically handle hex conversion
@@ -48,7 +54,7 @@ def main():
 
     print(algo.flash_info)
 
-    template_dir = os.path.dirname(os.path.realpath(__file__))
+    template_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates')
     output_dir = os.path.dirname(args.elf_path)
 
     # Allocate stack after algo and its rw data, rounded up.
@@ -63,14 +69,7 @@ def main():
         'stack_pointer': SP,
     }
 
-    tmpl_name_list = [
-        ("c_blob.tmpl", "c_blob.c"),
-        ("py_blob_orig.tmpl", "py_blob_orig.py"),
-        ("py_blob.tmpl", "py_blob.py"),
-        ("c_blob_mbed.tmpl", "c_blob_mbed.c")
-    ]
-
-    for tmpl, name in tmpl_name_list:
+    for tmpl, name in TEMPLATES:
         template_path = os.path.join(template_dir, tmpl)
         output_path = os.path.join(output_dir, name)
         algo.process_template(template_path, output_path, data_dict)
